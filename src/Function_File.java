87e6231b1e7a6d9b2bcaa918a0f48e4e8a87437e
rmzKhatib <ramzikhatib07@gmail.com>
2024-08-10 05:41:40 -0400

    functions added to "open"

--- src/Function_File.java
+++ src/Function_File.java
@@ -1,8 +1,12 @@
 import java.awt.FileDialog;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Function_File {
 
     GUI gui;
+    String fileName;
+    String fileAddress;
 
     public Function_File(GUI gui) {
 
@@ -17,6 +21,32 @@ public class Function_File {
 
         FileDialog fd = new FileDialog(gui.window, "Open", FileDialog.LOAD);
         fd.setVisible(true);
+
+        if(fd.getFile() != null) {
+            fileName = fd.getFile();
+            fileAddress = fd.getDirectory();
+            gui.window.setTitle(fileName);
+        }
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileAddress + fileName));
+
+            gui.textArea.setText("");
+
+            String line = null;
+
+            while((line = br.readLine()) != null) {
+
+                gui.textArea.append(line + "\n");
+
+            }
+            br.close();
+            
+        }catch(Exception e) {
+
+            System.out.println("ERROR...FIlE NOT OPENED");
+
+        }
     }
 
 }
